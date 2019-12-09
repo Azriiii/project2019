2349deebc5bac910ccbae0c80e8169a7576e7c79
amine9898 <47857678+amine9898@users.noreply.github.com>
2019-12-09 12:36:39 +0000

    Update Cookie.php

--- entities/Cookie.php
+++ entities/Cookie.php
@@ -22,7 +22,7 @@ function encrypt_decrypt($action, $string) {
     // hash
     $key = hash('sha256', $secret_key);
     
-    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+    // iv -encrypt methode Qr
     $iv = substr(hash('sha256', $secret_iv), 0, 16);
     if ( $action == 'encrypt' ) {
         $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
